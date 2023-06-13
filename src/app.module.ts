b66094203c0c056a2aba32ad95dad293611c40cb
Shoeb Uddin Ahmed <67792649+shoeb-gg@users.noreply.github.com>
2023-06-13 13:24:11 +0600

    Revert "server module for deployment"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 
 import { CoreModule } from './core/core.module';
 import { PartnerModule } from './partner/partner.module';
 import { CustomerModule } from './customer/customer.module';
 
 @Module({
-  imports: [
-    CoreModule,
-    PartnerModule,
-    CustomerModule,
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'client'),
-    }),
-  ],
+  imports: [CoreModule, PartnerModule, CustomerModule],
   controllers: [],
   providers: [],
 })
