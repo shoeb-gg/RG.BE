7e1adbd7e8f032608a22d30f0b919548fe9e910f
Shoeb Uddin Ahmed <shoeb.ua@gmail.com>
2023-06-13 14:57:38 +0600

    rm static server module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 
 import { CoreModule } from './core/core.module';
@@ -7,14 +6,7 @@ import { PartnerModule } from './partner/partner.module';
 import { CustomerModule } from './customer/customer.module';
 
 @Module({
-  imports: [
-    CoreModule,
-    PartnerModule,
-    CustomerModule,
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'client'),
-    }),
-  ],
+  imports: [CoreModule, PartnerModule, CustomerModule],
   controllers: [],
   providers: [],
 })
