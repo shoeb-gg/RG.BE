c71b6f72714552af1fc05f9929075bb15bccc5e8
hasan-sagar <khsagar0512@gmail.com>
2023-06-23 18:48:31 +0600

    vehicle documents endpoints added

---
+++ src/common/dto/vehicle-documents.dto.ts
@@ -0,0 +1,8 @@
+
+export class VehicleDocumentsDto {
+    id: any;
+    bluebook_photo_url?: string
+    tax_token_photo_url?: string
+    fitness_token_photo_url?: string
+    insurance_photo_url?: string
+}
