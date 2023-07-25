1beb460071c6d69ee5a464a9226c2cadf8fa829a
hasan-sagar <khsagar0512@gmail.com>
2023-07-25 21:27:54 +0600

    Partner payment module added

---
+++ src/common/dto/partner-payment-details.dto.ts
@@ -0,0 +1,8 @@
+export class PartnerPaymentDetailsDto {
+  id: any;
+  account_type: string;
+  mfs_number?: string;
+  bank_account_number?: string;
+  bank_name?: string;
+  bank_branch_routing_number?: string;
+}
---
+++ src/partner/management/payment-details/payment-details.controller.ts
@@ -0,0 +1,52 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { PaymentDetailsService } from './payment-details.service';
+import { PartnerPaymentDetailsDto } from 'src/common/dto/partner-payment-details.dto';
+import { successResponse } from 'src/common/models/res.success';
+import { Roles } from 'src/core/jwt-roles/roles.decorator';
+import { JwtAuthGuard } from 'src/core/jwt-roles/jwt-auth.guard';
+import { RoleGuard } from 'src/core/jwt-roles/roles.guard';
+import { User } from 'src/common/decorators/user.decorator';
+
+@Controller('payment-details')
+export class PaymentDetailsController {
+  constructor(private readonly paymentService: PaymentDetailsService) {}
+
+  @Roles('user')
+  @UseGuards(JwtAuthGuard, RoleGuard)
+  @Post()
+  async upsertPaymentDetails(
+    @User() user,
+    @Body() paymentDetails: PartnerPaymentDetailsDto,
+  ): Promise<successResponse> {
+    return await this.paymentService.upsertPaymentDetails(
+      user.userId,
+      paymentDetails,
+    );
+  }
+
+  @Roles('user')
+  @UseGuards(JwtAuthGuard, RoleGuard)
+  @Delete(':paymentDetailsId')
+  async deletePaymentDetails(
+    @Param('paymentDetailsId') paymentDetailsId: string,
+  ): Promise<successResponse> {
+    return this.paymentService.deletePaymentDetails(paymentDetailsId);
+  }
+
+  @Roles('user')
+  @UseGuards(JwtAuthGuard, RoleGuard)
+  @Get(':paymentDetailsId')
+  async getPaymentDetails(
+    @Param('paymentDetailsId') paymentDetailsId: string,
+  ): Promise<successResponse> {
+    return this.paymentService.getPaymentDetails(paymentDetailsId);
+  }
+}
